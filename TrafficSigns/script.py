6bf732901bdc3334a5b2efe70c0c5695038c3b4a
Caleb Tseng-Tham <cztsengt@uwaterloo.ca>
2019-03-04 13:38:57 -0500

    new tests

---
+++ TrafficSigns/script.py
@@ -0,0 +1,34 @@
+import numpy as np
+import tensorflow as tf
+import os.path
+import os
+
+def load_data(data_directory):
+    directories = [d for d in os.listdir(data_directory)
+                   if os.path.isdir(os.path.join(data_directory, d))]
+    labels = []
+    images = []
+    for d in directories:
+        label_directory = os.path.join(data_directory, d)
+        file_names = [os.path.join(label_directory, f)
+                      for f in os.listdir(label_directory)
+                      if f.endswith(".ppm")]
+        for f in file_names:
+            images.append(skimage.data.imread(f))
+            labels.append(int(d))
+    return images, labels
+
+ROOT_PATH = "TrafficSigns"
+train_data_directory = os.path.join(ROOT_PATH, "TrafficSigns/Training")
+test_data_directory = os.path.join(ROOT_PATH, "TrafficSigns/Testing")
+
+images, labels = load_data(train_data_directory)
+
+# Print the `images` dimensions
+print(images.ndim)
+
+# Print the number of `images`'s elements
+print(images.size)
+
+# Print the first instance of `images`
+images[0]
